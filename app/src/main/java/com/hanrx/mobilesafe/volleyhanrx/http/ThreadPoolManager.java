68d233c386e51d113e281f4f0d2d4b5a9265d243
wangjp0712 <wangjp0712@thundersoft.com>
2020-02-24 23:47:22 +0800

    last commit

---
+++ app/src/main/java/com/hanrx/mobilesafe/volleyhanrx/http/ThreadPoolManager.java
@@ -0,0 +1,69 @@
+package com.hanrx.mobilesafe.volleyhanrx.http;
+
+import android.util.Log;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.Future;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.RejectedExecutionHandler;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class ThreadPoolManager {
+
+    private static final String TAG = "ThreadPoolManager";
+
+    private static ThreadPoolManager instance = new ThreadPoolManager();
+
+    private LinkedBlockingDeque<Future<?>> taskQueue = new LinkedBlockingDeque<>();
+
+    private ThreadPoolExecutor threadPoolExecutor;
+
+    public static ThreadPoolManager getInstance() {
+        return instance;
+    }
+
+    private ThreadPoolManager() {
+        threadPoolExecutor = new ThreadPoolExecutor(4,10,10,
+                TimeUnit.SECONDS,new ArrayBlockingQueue<Runnable>(4), handler);
+        threadPoolExecutor.execute(runnable);
+    }
+
+    private Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            while (true) {
+                FutureTask futureTask = null;
+                try {
+                    //阻塞式函数
+                    Log.i(TAG,"等待队列   " + taskQueue.size());
+                    futureTask = (FutureTask) taskQueue.take();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                if (futureTask != null) {
+                    threadPoolExecutor.execute(futureTask);
+                }
+                Log.i(TAG,"线程池大小   " + threadPoolExecutor.getPoolSize());
+            }
+        }
+    };
+
+    public <T> void execte(FutureTask<T> futureTask) throws InterruptedException{
+        taskQueue.put(futureTask);
+    }
+
+    private RejectedExecutionHandler handler = new RejectedExecutionHandler() {
+        @Override
+        public void rejectedExecution(Runnable runnable, ThreadPoolExecutor threadPoolExecutor) {
+            try {
+                taskQueue.put(new FutureTask(runnable, null) {
+
+                });
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    };
+}
--- app/src/main/java/com/hanrx/mobilesafe/volleyhanrx/http/Volley.java
+++ app/src/main/java/com/hanrx/mobilesafe/volleyhanrx/http/Volley.java
@@ -1,12 +1,31 @@
 package com.hanrx.mobilesafe.volleyhanrx.http;
 
+import com.hanrx.mobilesafe.volleyhanrx.R;
+import com.hanrx.mobilesafe.volleyhanrx.http.interfaces.IDataListener;
+import com.hanrx.mobilesafe.volleyhanrx.http.interfaces.IHttpListener;
+import com.hanrx.mobilesafe.volleyhanrx.http.interfaces.IHttpService;
+
+import java.util.concurrent.FutureTask;
+
 public class Volley {
     /**
-     *
+     *  暴露给调用层
      * @param <T> 请求参数类型
      * @param <M> 响应参数类型
      */
-    public static <T, M> void sendRequest() {
-
+    public static <T, M> void sendRequest(T requestInfo, String url,
+                                          Class<M> response, IDataListener dataListener) {
+        RequestHolder<T> requestHolder = new RequestHolder<>();
+        requestHolder.setUrl(url);
+        IHttpService httpService = new JsonHttpService();
+        IHttpListener httpListener = new JsonDealListener<>(response, dataListener);
+        requestHolder.setHttpService(httpService);
+        requestHolder.setHttpListener(httpListener);
+        HttpTask<T> httpTask = new HttpTask<>(requestHolder);
+        try {
+            ThreadPoolManager.getInstance().execte(new FutureTask<Object>(httpTask, null));
+        } catch (InterruptedException e) {
+            dataListener.onFail();
+        }
     }
 }
